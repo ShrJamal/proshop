e8498f259da2c67138b4e375b520339eb07ce5df
Jamal .S <me@shrjamal.com>
2021-01-06 12:34:56 +0100

    add auth middlware

---
+++ backend/middleware/auth.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    let token = '';
+    const authorization = req.header('Authorization');
+    if (authorization?.startsWith('Bearer')) {
+      token = authorization.split(' ')[1];
+    }
+    if (!token) throw new Error('No Authorization no Token');
+    const decoded: any = jwt.verify(token, process.env.JWT_SECRET);
+    req.body._id = decoded._id;
+    next();
+  } catch (err) {
+    res.status(401);
+    next(err);
+  }
+}
--- backend/middleware/error.ts
+++ backend/middleware/error.ts
@@ -1,13 +1,20 @@
-export function notFound(req, res, next) {
-  const err = new Error('Not Found - ' + req.originalUrl);
+import { Request, Response, NextFunction } from 'express';
+
+export function notFound(req: Request, res: Response, next: NextFunction) {
   res.status(404);
-  next(err);
+  next(new Error('EndPoint Not Found - ' + req.originalUrl));
 }
 
-export function errorHandler(err, _, res, __) {
-  res.status(res.statusCode === 200 ? 500 : res.statusCode);
-  res.json({
-    message: err.message,
+export function errorHandler(
+  err: any,
+  _: Request,
+  res: Response,
+  __: NextFunction,
+) {
+  console.error(`errorHandler ${err}`.red.underline);
+  res.status(res.statusCode === 200 ? 500 : res.statusCode).json({
+    message: err.message ?? err,
+    code: err.code,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
 }
