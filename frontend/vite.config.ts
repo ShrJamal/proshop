288b46f1bd16fb75de28f8482476a131d83aa743
Jamal .S <me@shrjamal.com>
2022-04-22 19:01:46 +0000

    config proxy

---
+++ frontend/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+  resolve: {
+    alias: {
+      '~': '/src/',
+      '@': '/src/components/',
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+})
